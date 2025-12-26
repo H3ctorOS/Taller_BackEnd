5325b17af9a2f2fc0c22fb7db6516c6aa8a55488
H3ctorOS <h3ctor.oss@gmail.com>
2025-12-26 08:03:19 +0000

    fachada clientes consultas

--- src/main/java/com/TallerWapo/dominio/contexto/ContextoDaos.java
+++ src/main/java/com/TallerWapo/dominio/contexto/ContextoDaos.java
@@ -1,12 +1,17 @@
 package com.TallerWapo.dominio.contexto;
 
+import com.TallerWapo.Adaptadores.BaseDatossql.daosImpl.ClientesSQLDaoImp;
 import com.TallerWapo.Adaptadores.BaseDatossql.daosImpl.VehiculoSQLDaoImp;
+import com.TallerWapo.dominio.interfaces.Daos.clientesDao;
 import com.TallerWapo.dominio.interfaces.Daos.vehiculosDao;
 
 public class ContextoDaos {
 
     public static vehiculosDao getVehiculoDao() {
         return new VehiculoSQLDaoImp();
+    }
+    public static clientesDao getClienteDao() {
+        return new ClientesSQLDaoImp();
     }
 
 }
--- src/main/java/com/TallerWapo/dominio/fachadas/negocio/clientes/ClientesFachadaConsultasImpl.java
+++ src/main/java/com/TallerWapo/dominio/fachadas/negocio/clientes/ClientesFachadaConsultasImpl.java
@@ -4,17 +4,17 @@ package com.TallerWapo.dominio.fachadas.negocio.clientes;
 import com.TallerWapo.dominio.BOs.Clientes.ClienteBO;
 import com.TallerWapo.dominio.contexto.ContextoDaos;
 import com.TallerWapo.dominio.fachadas.base.FachadaConsultaBase;
-import com.TallerWapo.dominio.interfaces.Daos.vehiculosDao;
+import com.TallerWapo.dominio.interfaces.Daos.clientesDao;
 
 import java.util.List;
 
 public class ClientesFachadaConsultasImpl extends FachadaConsultaBase {
-    private final vehiculosDao vehiculoDao = ContextoDaos.getVehiculoDao();
+    private final clientesDao clientesDao = ContextoDaos.getClienteDao();
 
     public ClienteBO buscarClienteDni(String dni) {
         return ejecutarConsulta(() -> {
             try {
-                return null;
+                return clientesDao.buscarPorDni(dni);
 
             } catch (Exception e) {
                 throw new RuntimeException(e);
@@ -25,7 +25,7 @@ public class ClientesFachadaConsultasImpl extends FachadaConsultaBase {
     public List<ClienteBO> buscarTodosLosClientes() {
         return ejecutarConsulta(() -> {
             try {
-                return null;
+                return clientesDao.buscarTodos();
 
             } catch (Exception e) {
                 throw new RuntimeException(e);
